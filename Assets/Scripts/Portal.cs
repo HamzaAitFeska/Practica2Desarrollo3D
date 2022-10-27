634c969b8d3beb0e332c215dd14ce9cfddde4654
HamzaAitFeska <77283740+HamzaAitFeska@users.noreply.github.com>
2022-10-27 17:15:44 +0200

    portal script

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Camera m_Camera;
+    public Transform m_OtherPortaktransform;
+    public Portal m_MirrorPortal;
+    public FPSPlayerController m_Player;
+
+    private void LateUpdate()
+    {
+        Vector3 l_WorldPosiition = m_Player.m_Camera.transform.position;
+        Vector3 l_localPosition = m_OtherPortaktransform.InverseTransformPoint(l_WorldPosiition);
+        m_MirrorPortal.m_Camera.transform.position = m_MirrorPortal.transform.TransformPoint(l_localPosition);
+
+        Vector3 l_WorldDirection = m_Player.m_Camera.transform.forward;
+        Vector3 l_localDirection = m_OtherPortaktransform.InverseTransformDirection(l_WorldDirection);
+        m_MirrorPortal.m_Camera.transform.forward = m_MirrorPortal.transform.TransformDirection(l_localDirection);
+    }
+}
