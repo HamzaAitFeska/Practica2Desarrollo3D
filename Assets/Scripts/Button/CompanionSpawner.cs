37d4f3f20bfad4504cfa72d4aa5750c133c65c14
EdouAAA <ehamon@edu.tecnocampus.cat>
2022-11-15 02:08:32 +0100

    Más de nivel

--- Assets/Scripts/Button/CompanionSpawner.cs
+++ Assets/Scripts/Button/CompanionSpawner.cs
@@ -6,10 +6,18 @@ public class CompanionSpawner : MonoBehaviour
 {
     public Companion m_prefabCompanion;
     public Transform m_SpawnPosition;
+    public GameObject m_Button;
+    Button button;
     // Start is called before the first frame update
-    private void OnTriggerEnter(Collider other)
+
+    bool doorIsOpen = false;
+    private void Start()
     {
-        if(other.tag == "Player")
+        button = m_Button.GetComponent<Button>();
+    }
+    private void Update()
+    {
+        if (button.m_ButtonIsPressed)
         {
             GameObject.Instantiate(m_prefabCompanion, m_SpawnPosition.position, m_SpawnPosition.rotation);
         }
